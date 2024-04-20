2cc6ceb771a61af0f82be4daf7bccfefeb784d10
AG3NTZ3R0 <Z3R0@duck.com>
2024-04-19 18:01:11 -0600

    Introduces a fn to construct URLs

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use tokio::io::AsyncWriteExt;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let url = "https://query1.finance.yahoo.com/v7/finance/download/NVDA?period1=1508112000&period2=1713561246&interval=1d&events=history&includeAdjustedClose=true";
+    let url = build_yahoo_finance_url("NVDA", 1508112000, 1713561246, "1d", true);
+
     let response = reqwest::get(url).await?;
 
     if response.status().is_success() {
@@ -17,4 +18,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+fn build_yahoo_finance_url(
+    symbol: &str,
+    period1: u64, 
+    period2: u64, 
+    interval: &str, 
+    include_adjusted_close: bool,
+) -> String {
+    format!(
+        "https://query1.finance.yahoo.com/v7/finance/download/{}?period1={}&period2={}&interval={}&events=history&includeAdjustedClose={}",
+        symbol,
+        period1,
+        period2,
+        interval,
+        include_adjusted_close
+    )
 }
