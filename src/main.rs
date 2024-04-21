ced68a6755677618b409081b60c23b79993aa05b
AG3NTZ3R0 <Z3R0@duck.com>
2024-04-21 09:35:45 -0600

    Introduces a fn to construct URLs from dates

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use chrono::{NaiveDate, Utc};
+use chrono::TimeZone;
 use std::error::Error;
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let url = build_yahoo_finance_url("NVDA", 1508112000, 1713561246, "1d", true);
+    let msg = "Failed to generate date";
+    let date1 = NaiveDate::from_ymd_opt(2023, 01, 01).expect(msg);
+    let date2 = NaiveDate::from_ymd_opt(2024, 01, 01).expect(msg);
+    let url = build_yahoo_finance_url_from_dates("NVDA", date1, date2, "1d", true);
 
     let response = reqwest::get(url).await?;
 
@@ -18,6 +23,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+fn build_yahoo_finance_url_from_dates(
+    symbol: &str,
+    date1: NaiveDate,
+    date2: NaiveDate,
+    interval: &str,
+    include_adjusted_close: bool,
+) -> String {
+    let msg = "Failed to generate timestamp from date";
+    let datetime1 = date1.and_hms_opt(0, 0, 0).expect(msg);
+    let datetime2 = date2.and_hms_opt(0, 0, 0).expect(msg);
+    
+    let period1 = Utc.from_utc_datetime(&datetime1).timestamp() as u64;
+    let period2 = Utc.from_utc_datetime(&datetime2).timestamp() as u64;
+    build_yahoo_finance_url(symbol, period1, period2, interval, include_adjusted_close)
 }
 
 fn build_yahoo_finance_url(
